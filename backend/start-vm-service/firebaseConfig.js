f1d78377f1876ce377d5e116e574c0834350fa12
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2025-05-06 21:39:08 -0400

    change to bad methode

--- backend/start-vm-service/firebaseConfig.js
+++ backend/start-vm-service/firebaseConfig.js
@@ -1,13 +1,24 @@
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.FIREBASE_DATABASE_URL,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID,
+//     measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+//     serviceAccountKeyPath: process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH
+// };
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    serviceAccountKeyPath: process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH
-};
+    apiKey: "AIzaSyDmdf8NhoFAzXKGuBWYq5XoDrM5eNClgOg",
+    authDomain: "bradensbay-1720893101514.firebaseapp.com",
+    databaseURL: "https://bradensbay-1720893101514-default-rtdb.firebaseio.com",
+    projectId: "bradensbay-1720893101514",
+    storageBucket: "bradensbay-1720893101514.firebasestorage.app",
+    messagingSenderId: "280971564912",
+    appId: "1:280971564912:web:989fff5191d0512c1b21b5",
+    measurementId: "G-DNJS8CVKWD",
+    serviceAccountKeyPath: "bradensbay-1720893101514-firebase-adminsdk-2j5xq-0b1f3a7c8e.json"
+  };
 
 module.exports = firebaseConfig;
